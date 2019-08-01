7408d746a733b084493c0e59cab8d8b16d47984b
ImperialMan <VladImpersky@Gmail.com>
2019-08-01 19:35:08 +0300

    Added common project and news class. TODO: Implement news UI

---
+++ src/Common/News.cs
@@ -0,0 +1,42 @@
+﻿namespace Common
+{
+    public class News
+    {
+        public string russianHeader;
+        public string englishHeader;
+
+        public string russianPreviewText;
+        public string englishPreviewText;
+
+        public string russianText;
+        public string englishText;
+
+        public NewsType newsType;
+
+        public long newsTime;
+
+        public News(string rusHeader, string rusPreviewText, string rusText,
+            string engHeader, string engPreviewText, string engText,
+            NewsType type, long time)
+        {
+            russianHeader = rusHeader;
+            englishHeader = engHeader;
+
+            russianPreviewText = rusPreviewText;
+            englishPreviewText = engPreviewText;
+
+            russianText = rusText;
+            englishText = engText;
+
+            newsType = type;
+            newsTime = time;
+        }
+    }
+
+    public enum NewsType
+    {
+        Tournament,
+        Update,
+        Event
+    }
+}
