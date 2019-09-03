12a8ad9253356ed02ae12776eb586130cb77fe37
Kholodov VA <vholodov94@gmail.com>
2019-09-03 22:10:24 +0300

    Stats table and players stat send

---
+++ src/GSMasterServer/Data/Entities/Stats/ProfileDetailDBO.cs
@@ -0,0 +1,177 @@
+﻿using LiteDB;
+
+namespace GSMasterServer.Data
+{
+    public class ProfileDetailDBO
+    {
+
+        public long Id { get; set; }
+        public long ProfileId { get; set; }
+        
+        public long SmGamesCount { get; set; }
+        public long CsmGamesCount { get; set; }
+        public long OrkGamesCount { get; set; }
+        public long EldarGamesCount { get; set; }
+        public long IgGamesCount { get; set; }
+        public long NecGamesCount { get; set; }
+        public long TauGamesCount { get; set; }
+        public long DeGamesCount { get; set; }
+        public long SobGamesCount { get; set; }
+
+        public long SmWinCount { get; set; }
+        
+        public long CsmWinCount { get; set; }
+        public long OrkWinCount { get; set; }
+        public long EldarWinCount { get; set; }
+        public long IgWinCount { get; set; }
+        public long NecrWinCount { get; set; }
+        public long TauWinCount { get; set; }
+        public long DeWinCount { get; set; }
+        public long SobWinCount { get; set; }
+        
+        public long SmGamesCountAuto { get; set; }
+        public long CsmGamesCountAuto { get; set; }
+        public long OrkGamesCountAuto { get; set; }
+        public long EldarGamesCountAuto { get; set; }
+        public long IgGamesCountAuto { get; set; }
+        public long NecrGamesCountAuto { get; set; }
+        public long TauGamesCountAuto { get; set; }
+        public long DeGamesCountAuto { get; set; }
+        public long SobGamesCountAuto { get; set; }
+
+        public long SmWinCountAuto { get; set; }
+        public long CsmWinCountAuto { get; set; }
+        public long OrkWinCountAuto { get; set; }
+        public long EldarWinCountAuto { get; set; }
+        public long IgWinCountAuto { get; set; }
+        public long NecrWinCountAuto { get; set; }
+        public long TauWinCountAuto { get; set; }
+        public long DeWinCountAuto { get; set; }
+        public long SobWinCountAuto { get; set; }
+
+        public ProfileDetailDBO()
+        {
+            
+        }
+        
+        public ProfileDetailDBO(long profileId)
+        {
+            ProfileId = profileId;
+        }
+
+        public ProfileDetailDBO updateRaceStat(Race raceType, bool isWin, bool isAuto)
+        {
+            switch (raceType)
+            {
+                case Race.space_marine_race:
+                    if (isAuto) SmGamesCountAuto++; else SmGamesCount++;
+                    if (isWin)
+                    {
+                        if (isAuto) SmWinCountAuto++; else SmWinCount++;
+                    }
+                    break;
+                case Race.chaos_marine_race:
+                    if (isAuto) CsmGamesCountAuto++; else CsmGamesCount++;
+                    if (isWin)
+                    {
+                        if (isAuto) CsmWinCountAuto++; else CsmWinCount++;
+                    }
+                    break;
+                case Race.ork_race:
+                    if (isAuto) OrkGamesCountAuto++; else OrkGamesCount++;
+                    if (isWin)
+                    {
+                        if (isAuto) OrkWinCountAuto++; else OrkWinCount++;
+                    }
+                    break;
+                case Race.eldar_race:
+                    if (isAuto) EldarGamesCountAuto++; else EldarGamesCount++;
+                    if (isWin)
+                    {
+                        if (isAuto) EldarWinCountAuto++; else EldarWinCount++;
+                    }
+                    break;
+                case Race.guard_race:
+                    if (isAuto) IgGamesCountAuto++; else IgGamesCount++;
+                    if (isWin)
+                    {
+                        if (isAuto) IgWinCountAuto++; else IgWinCount++;
+                    }
+                    break;
+                case Race.necron_race:
+                    if (isAuto) NecrGamesCountAuto++; else NecGamesCount++;
+                    if (isWin)
+                    {
+                        if (isAuto) NecrWinCountAuto++; else NecrWinCount++;
+                    }
+                    break;
+                case Race.tau_race:
+                    if (isAuto) TauGamesCountAuto++; else TauGamesCount++;
+                    if (isWin)
+                    {
+                        if (isAuto) TauWinCountAuto++; else TauWinCount++;
+                    }
+                    break;
+                case Race.dark_eldar_race:
+                    if (isAuto) DeGamesCountAuto++; else DeGamesCount++;
+                    if (isWin)
+                    {
+                        if (isAuto) DeWinCountAuto++; else DeWinCount++;
+                    }
+                    break;
+                case Race.sisters_race:
+                    if (isAuto) SobGamesCountAuto++; else SobGamesCount++;
+                    if (isWin)
+                    {
+                        if (isAuto) SobWinCountAuto++; else SobWinCount++;
+                    }
+                    break;
+            }
+
+            return this;
+        }
+    }
+
+    public class Profile1X1DBO : ProfileDetailDBO
+    {
+
+        public Profile1X1DBO() : base()
+        {
+            
+        }
+        public Profile1X1DBO(long profileId): base(profileId)
+        {
+        }
+    }
+    
+    public class Profile2X2DBO : ProfileDetailDBO
+    {
+        public Profile2X2DBO() : base()
+        {
+            
+        }
+        public Profile2X2DBO(long profileId) : base(profileId)
+        {
+        }
+    }
+    public class Profile3X3DBO : ProfileDetailDBO
+    {
+        public Profile3X3DBO() : base()
+        {
+            
+        }
+        public Profile3X3DBO(long profileId) : base(profileId)
+        {
+        }
+    }
+    public class Profile4X4DBO : ProfileDetailDBO
+    {
+        public Profile4X4DBO() : base()
+        {
+            
+        }
+        public Profile4X4DBO(long profileId) : base(profileId)
+        {
+        }
+    }
+}
--- src/GSMasterServer/Services/Interfaces/IMainDataBase.cs
+++ src/GSMasterServer/Services/Interfaces/IMainDataBase.cs
@@ -21,6 +21,19 @@ namespace GSMasterServer.Services
         ProfileDBO[] Load1v1Top10();
         ProfileDBO[] LoadAllStats();
 
+        Profile1X1DBO GetProfile1X1ByProfileId(long profileId);
+        Profile2X2DBO GetProfile2X2ByProfileId(long profileId);
+        Profile3X3DBO GetProfile3X3ByProfileId(long profileId);
+        Profile4X4DBO GetProfile4X4ByProfileId(long profileId);
+        Profile1X1DBO CreateProfile1X1(long profileId);
+        Profile2X2DBO CreateProfile2X2(long profileId);
+        Profile3X3DBO CreateProfile3X3(long profileId);
+        Profile4X4DBO CreateProfile4X4(long profileId);
+        void UpdateProfile1X1(Profile1X1DBO profile);
+        void UpdateProfile2X2(Profile2X2DBO profile);
+        void UpdateProfile3X3(Profile3X3DBO profile);
+        void UpdateProfile4X4(Profile4X4DBO profile);
+
         NewsDBO[] GetLastNews(int count);
     }
 }
