b94389fb1689b9113454b9bbdcc9a0b8fa512f8d
ImperialMan <VladImpersky@Gmail.com>
2019-09-05 19:20:41 +0300

    Fixed Program.cs logger

--- src/GSMasterServer/Program.cs
+++ src/GSMasterServer/Program.cs
@@ -1,4 +1,5 @@
 ﻿using GSMasterServer.Servers;
+using GSMasterServer.Services;
 using System;
 using System.Net;
 using System.Threading;
@@ -6,10 +7,7 @@ using System.Threading;
 namespace GSMasterServer
 {
     class Program
-	{
-
-        private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
-        
+	{     
         static void Main(string[] args)
         {
             args = new string[] { "+db", @"test.db" };
@@ -24,14 +22,14 @@ namespace GSMasterServer
                     {
                         if ((i >= args.Length - 1) || !IPAddress.TryParse(args[i + 1], out bind))
                         {
-                            logger.Error("+bind value must be a valid IP Address to bind to!");
+                            Logger.Error("+bind value must be a valid IP Address to bind to!");
                         }
                     }
                     else if (args[i].Equals("+db"))
                     {
                         if ((i >= args.Length - 1))
                         {
-                            logger.Error("+db value must be a path to the database");
+                            Logger.Error("+db value must be a path to the database");
                         }
                         else
                         {
@@ -43,12 +41,12 @@ namespace GSMasterServer
 
             if (!Database.IsInitialized())
             {
-                logger.Error("Error initializing database, please confirm parameter +db is valid");
-                logger.Error("Press any key to continue");
-                Console.ReadKey();
+                Logger.Fatal("Error initializing database, please confirm parameter +db is valid");
+                //Logger.Error("Press any key to continue");
+                //Console.ReadKey();
                 return;
             }
-            logger.Info("Database successful initialized");
+            Logger.Info("Database successful initialized");
 
             CDKeyServer cdKeyServer = new CDKeyServer(bind, 29910);
             //ServerListReport serverListReport = new ServerListReport(bind, 27900);
